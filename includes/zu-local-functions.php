07bd5fd7e8fa1c1500d661666622735d5457fe37
zu1952 <erkin.zadauly@gmail.com>
2020-08-27 11:23:27 +0600

    Прилизал
    Перенес функции в файл функций и подробно прокомментировал

--- includes/zu-local-functions.php
+++ includes/zu-local-functions.php
@@ -1,13 +1,24 @@
 <?php
-// Отправляем на консоль сообщение, что сайт работает локально
-function zu_local_add_promt($param = '/time:5', $txt = 'Server started locally') {
+/*
+ * Посылка сообщения на консоль *
+ ********************************
+ * Если сайт работает локально отправляется на консоль  
+ * сообщение $txt с параметрами $param.
+ * 
+ * Запуск функции без аргументов:
+ *    выводится текст о том, что сервер запущен локально;
+ *    окно сообщения автоматически закрывается через 5 сек.
+ * **************************************************************/
+
+ function zu_local_add_promt($param = '/time:5', $txt = 'Server started locally') {
     if ($_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR']) {
         $cmd = 'msg console';
         execInBackground($cmd, $param, __($txt));
     }
-}
+};
 
-// Исполнение команд локально
+// Исполнение команд локально.
+// Проверено пока только на Windows.
 function execInBackground($cmd, $param, $txt = '') {
     if (substr(php_uname(), 0, 7) == "Windows"){
         pclose(popen($cmd . ' ' . $param . ' ' . $txt, "r")); 
@@ -15,5 +26,32 @@ function execInBackground($cmd, $param, $txt = '') {
     else {
         exec($cmd . ' ' . $param . ' ' . $txt . " > /dev/null &");  
     }
-}
+};
 
+// Подключение функций.
+function zu_local_include() {
+    $zu_local_functions_path = plugin_dir_path(__FILE__);
+    $zu_local_functions_path .= 'includes/zu-local-functions.php';
+    return '<?php recuire_once ' . $zu_local_functions_path . '?>';
+};
+
+// Включение подпункта "ZU Local" к меню "Инструменты" и
+// регистрация функции отображения страницы плагина.
+function register_zu_local_page() {
+    add_submenu_page( 'tools.php', 
+        __('Local Debug'), 
+        __('Debug MSG'),
+        'manage_options', 
+        'zu-local-page', 
+        'zu_local_page_callback' 
+    ); 
+};
+
+// Отображение страницы плагина.
+function zu_local_page_callback() {
+	// контент страницы
+	echo '<div class="wrap">';
+    echo '<h2>'. get_admin_page_title() .'</h2>';
+    echo '<p>Страница управления локальной отладкой.</p>';
+	echo '</div>';
+};
--- zu-local.php
+++ zu-local.php
@@ -9,12 +9,13 @@ Author: Erkin Zadauly
 // Подключаем zu-local-functions.php
 require_once plugin_dir_path(__FILE__) . 'includes/zu-local-functions.php';
 
-/* 
-Активация плагина
-*/
-
-// После активации выводит сообщение
+// Активация плагина.
 register_activation_hook( __FILE__, 'zu_local_activation' );
+
+// При активации запускается функция вывода сообщения.
+function zu_local_activation() {
+    zu_local_add_promt();
+};
 
 // Выводит после рендеринга страницы сообщение
 add_action('wp_footer', 'zu_local_add_promt');
@@ -22,33 +23,5 @@ add_action('wp_footer', 'zu_local_add_promt');
 // Включает на все страницы функции
 add_action('wp_header', 'zu_local_include');
 
-function zu_local_activation() {
-    zu_local_add_promt();
-}
-
-function zu_local_include() {
-    $zu_local_functions_path = plugin_dir_path(__FILE__);
-    $zu_local_functions_path .= 'includes/zu-local-functions.php';
-    return '<?php recuire_once ' . $zu_local_functions_path . '?>';
-}
-
 // Добавим подменю в меню админ-панели "Инструменты" (tools):
 add_action('admin_menu', 'register_zu_local_page');
-
-function register_zu_local_page() {
-    add_submenu_page( 'tools.php', 
-        __('Local Debug'), 
-        __('Debug MSG'),
-        'manage_options', 
-        'zu-local-page', 
-        'zu_local_page_callback' 
-    ); 
-}
-
-function zu_local_page_callback() {
-	// контент страницы
-	echo '<div class="wrap">';
-    echo '<h2>'. get_admin_page_title() .'</h2>';
-    echo '<p>Страница управления локальной отладкой.</p>';
-	echo '</div>';
-}
